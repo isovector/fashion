ff780ff04a7ff24d7421f532f07203a6266e3dc4
Sandy Maguire <sandy@sandymaguire.me>
2019-07-08 20:32:11 -0400

    haltons

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,17 +2,24 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE GADTs                     #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeApplications          #-}
 
 module Lib where
 
--- $> :set args -w 350 -h 350 -o out.svg
-
--- $> main
-
+import Data.Bool
+import Control.Monad.ST
+import Data.STRef
+import Diagrams.Core.Types
+import Control.Monad
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import qualified Diagrams.Color.XKCD as X
 import Web.Suavemente
+import Data.Function (fix)
+
+-- $> :set args -w 350 -h 350 -o out.svg
+
+-- $> main
 
 
 main :: IO ()
@@ -35,6 +42,9 @@ zoop p d = d # deform' 0.001 (wibble p) # rotateBy p
 -- blue: darkishBlue
 -- orange: brightOrange
 
+darkFlowerCol :: AlphaColour Double
+darkFlowerCol = darken 0.5 X.darkBlueGrey
+
 splotch :: Double -> AlphaColour Double -> Double -> Diagram B
 splotch sz c d = circle sz # zoop d # stroke # fillColor c
 
@@ -44,28 +54,72 @@ flowerSplotch n c px = rotateBy (cos $ fromIntegral n) $ bordering $ mconcat $ d
   let d = splotch 1 c (fromIntegral ix + px) # rotateBy (fromIntegral ix / fromIntegral n) # scaleX 2 # alignL # translateX 0.3
   pure $ d # rotateBy (fromIntegral ix / fromIntegral n)
 
+
+oneHalton :: Int -> Int -> Double
+oneHalton b ix = runST $ do
+  f <- newSTRef @Double 1
+  r <- newSTRef @Double 0
+  i <- newSTRef ix
+
+  fix $ \me -> do
+    ival <- readSTRef i
+    when (ival > 0) $ do
+      modifySTRef f (/ fromIntegral b)
+      fval <- readSTRef f
+      ival <- readSTRef i
+      modifySTRef r $ (+ (fval * fromIntegral (ival `mod` b)))
+      modifySTRef i $ fromIntegral . floor . (/ fromIntegral b) . fromIntegral
+      me
+
+  readSTRef r
+
+
+halton :: Int -> Int -> [V2 Double]
+halton x y = drop 1 $ do
+  i <- [0..]
+  pure $ (oneHalton x i - 0.5) ^& (oneHalton y i - 0.5)
+
+
+
 bordering :: Diagram B -> Diagram B
-bordering d = d # lw none <> d # scale 1.2 # lw 10 # lineColor X.darkBlueGrey
+bordering d = d # lw none <> d # scale 1.2 # lw 5 # lineColor darkFlowerCol
 
 flower :: Double -> Diagram B
 flower d = mconcat
-  [ splotch 1.8 X.darkBlueGrey d # lw none
-  , splotch 3 X.windowsBlue (d + 0.2) # lw 10 # lineColor X.darkBlueGrey
+  [ splotch 1.5 darkFlowerCol d # lw none
+  , splotch 3 X.windowsBlue (d + 0.2) # lw 5 # lineColor darkFlowerCol
   ]
 
-flower2 :: Double -> Diagram B
-flower2 d = mconcat
+flower2 :: Int -> Double -> Diagram B
+flower2 n d = mconcat
   [ splotch 0.5 X.windowsBlue d # lw none
-  , flowerSplotch 6 X.darkBlueGrey (d + 2) # scale 0.4
-  , flowerSplotch 6 X.windowsBlue d
+  , flowerSplotch n darkFlowerCol (d + 2) # scale 0.4
+  , flowerSplotch n X.windowsBlue d
   ]
+
+flowers :: Int -> Diagram B
+flowers n = mconcat $ do
+  (i, h) <- zip [0..n] $ halton 2 3
+  let p = (i * 7) `mod` 5 + 3
+      z = (i * 1394) `mod` 100 > 63
+      d = fromIntegral i * 0.37
+  pure $ bool (flower2 p d) (flower d) z # translate (h * 50)
+
 
 example :: Diagram B
 example = mconcat
-  [ flower2 0.5
-  , roundedRect 100 100 0.1 # fillColor X.brightOrange # rectEnvelope (0^&0) (0^&5) # centerXY
+  [ flowers 10
+  , orangeSplotch 0.1 # translateY 10 # rectEnvelope (0^&0) (0^&30) # translateY 10 # rotateBy 0.09
+  , roundedRect 100 100 0.1 # fillColor (darken 0.4 X.blueberry) # rectEnvelope (0^&0) (0^&5) # centerXY
   ]
 
+
+orangeSplotch :: Double -> Diagram B
+orangeSplotch d = mconcat
+  [ splotch 3 X.brightOrange d # lw none # alignL # scale 3 # translateX 2.9 # translateY 3
+  , splotch 3 X.brightOrange (d + 0.1) # lw none # scale 3 # translateY (-2)
+  , splotch 3 X.brightOrange (d + 0.2) # lw none # alignR # scale 3 # translateX (-2.9)
+  ] # scale (sin (d * tau) + 1) # rotateBy (cos d)
 
 sq n = square n
         # lw none
